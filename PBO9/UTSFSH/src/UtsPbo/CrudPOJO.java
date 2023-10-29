1d717377fcac8ec0abb2f02b3c4748f6348e8143
Varatlan <148843870+Varatlan@users.noreply.github.com>
2023-10-29 11:47:44 +0700

    Add files via upload

---
+++ PBO9/UTSFSH/src/UtsPbo/CrudPOJO.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package UtsPbo;
+
+public class CrudPOJO {
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getJudulbuku() {
+        return judulbuku;
+    }
+
+    public void setJudulbuku(String judulbuku) {
+        this.judulbuku = judulbuku;
+    }
+
+    public String getTahunterbit() {
+        return tahunterbit;
+    }
+
+    public void setTahunterbit(String tahunterbit) {
+        this.tahunterbit = tahunterbit;
+    }
+
+    public String getPenerbit() {
+        return penerbit;
+    }
+
+    public void setPenerbit(String penerbit) {
+        this.penerbit = penerbit;
+    }
+    private String isbn;
+    private String judulbuku;
+    private String tahunterbit;
+    private String penerbit;
+}
